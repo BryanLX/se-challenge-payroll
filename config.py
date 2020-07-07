55fc70c9322f98df67fec47861a5a560e4ed0d05
BryanLX <517591210@qq.com>
2020-07-06 21:39:10 -0400

    Finalize it

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config(object):
     UPLOAD_FILE_FOLDER = "files"
-    MSSQL_DATABASE_CONNECTION_STRING= "driver={ODBC Driver 17 for SQL Server};server=database-1.cefxvqk3sole.ca-central-1.rds.amazonaws.com,1433; database=PayRoLL; uid=adminbryan; pwd=adminadmin"
+    MSSQL_DATABASE_CONNECTION_STRING= ""
